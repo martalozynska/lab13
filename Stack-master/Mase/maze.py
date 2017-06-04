cbb769e38dbfde08a0bff7619beb4ca1424dc899
martalozynska <m.lozynska@ucu.edu.ua>
2017-06-04 23:33:49 +0300

    .

--- Stack-master/Mase/maze.py
+++ Stack-master/Mase/maze.py
@@ -43,7 +43,26 @@ class Maze :
     # Attempts to solve the maze by finding a path from the starting cell
     # to the exit. Returns True if a path is found and False otherwise.
     def findPath( self ):
-        pass
+        stack = Stack()
+        stack.push(self._startCell)
+        while stack.isEmpty() != True:
+            used_cell = stack.pop()
+            self._markTried(used_cell.row, used_cell.col)
+            if self._exitFound(used_cell.row,used_cell.col ):
+                used_cell.path.append(used_cell)
+                for cell in used_cell.path:
+                    self._markPath(cell.row, cell.col)
+                return True
+            if self._validMove(used_cell.row - 1, used_cell.col):
+                stack.push(_CellPosition(used_cell.row - 1, used_cell.col, used_cell))
+            if self._validMove(used_cell.row + 1, used_cell.col):
+                stack.push(_CellPosition(used_cell.row + 1, used_cell.col, used_cell))
+            if self._validMove(used_cell.row, used_cell.col - 1):
+                stack.push(_CellPosition(used_cell.row , used_cell.col -1, used_cell))
+            if self._validMove(used_cell.row, used_cell.col + 1):
+                stack.push(_CellPosition(used_cell.row, used_cell.col + 1, used_cell))
+
+        return False
 
     # Resets the maze by removing all "path" and "tried" tokens.
     def reset( self ):
@@ -55,7 +74,12 @@ class Maze :
 
     # Prints a text-based representation of the maze.
     def draw( self ):
-        pass
+        for row in range(self.num_rows()):
+            for col in range(self.num_cols()):
+                if self._mazeCells[row,col] == None:
+                    self._mazeCells[row,col] = ' '
+                print(self._mazeCells[row,col], end='')
+            print(' ')
 
     # Returns True if the given cell position is a valid move.
     def _validMove( self, row, col ):
@@ -77,9 +101,9 @@ class Maze :
 
 # Private storage class for holding a cell position.
 class _CellPosition( object ):
-    def __init__( self, row, col ):
+    def __init__( self, row, col, element=None ):
         self.row = row
         self.col = col
-
-m = Maze(3,4)
-print(m.reset())
+        self.path = []
+        if element != None:
+            self.path.append(element)
